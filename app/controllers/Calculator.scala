8c71c5c2bd0395be80f9ae6bf50700eeadf43c5d
Devon Stewart <blast@hardchee.se>
2013-08-21 20:26:16 +0900

    Refactoring to use Form and form helpers

--- app/controllers/Calculator.scala
+++ app/controllers/Calculator.scala
@@ -2,13 +2,26 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+case class CalculatorEquation(equation: String)
 
 object Calculator extends Controller {
+  val calculatorForm = Form(
+    mapping(
+      "equation" -> nonEmptyText
+    )(CalculatorEquation.apply)(CalculatorEquation.unapply)
+  )
+
   def index = Action {
-    Ok(views.html.calculatorIndex("Please enter an equation to evaluate!"))
+    Ok(views.html.calculatorIndex("Please enter an equation to evaluate!", calculatorForm))
   }
 
-  def calculate = Action {
-    Ok(views.html.calculatorIndex("Received POST"))
+  def calculate = Action { implicit request =>
+    calculatorForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.calculatorIndex("Invalid equation", formWithErrors)),
+      wrappedEquation => Ok(views.html.calculatorIndex("Found equation: " + wrappedEquation.equation, calculatorForm))
+    )
   }
 }
